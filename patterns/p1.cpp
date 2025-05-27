78808325e2cb4f9294324eee63fa447c8a418a0d
Prerit <preritd070@gmail.com>
2025-05-27 22:45:01 +0530

    pushing basic pattern code

---
+++ patterns/p1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T;
+    cout<<"Enter the number of test cases : ";
+    cin>>T;
+    while(T--){
+        int n;
+        cout<<"Enter the value of n : ";
+        cin>>n;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+    }
+}
+/*
+********
+********
+********
+********
+********
+********
+********
+********
+*/
